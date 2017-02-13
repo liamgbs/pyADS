d13248300d6eb3c06b332e59f82ecb9e8b79a816
Liam <liam.gibbings@cloudsolutions.co.uk>
2017-02-13 11:42:50 +0000

    Fixed linked list nested class problems, reorganised files with tests dir and structured like a proper python package.

---
+++ pyads/array.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+class Array:
+    """Array object, behaves like a Java array."""
+    def __init__(self, dimension=1, *size):
+        if len(size) != dimension:
+            raise self.ArraySizeMissingException()
+
+        if size[0] < 0:
+            raise self.NegativeArraySizeException()
+
+        if dimension < 1:
+            raise self.DimensionsOutOfBoundsException()
+        elif dimension > 1:
+            # Populate arrays with other arrays until bottom dimension is reached.
+            self._container = [Array(dimension - 1, *size[:dimension - 1]) for index in range(size[0])]
+        else:
+            self._container = [None] * size[0]
+
+    def __getitem__(self, key):
+        """Defines indexed getting behaviour for objects of this class."""
+        if key > len(self._container) - 1:
+            raise self.ArrayOutOfBoundsException()
+        elif self._container[key] is None:
+            raise self.NonePointerException()
+        else:
+            return self._container[key]
+
+    def __setitem__(self, key, data):
+        """Defines indexed setting behaviour for objects of this class."""
+        if key > len(self._container) - 1:
+            raise self.ArrayOutOfBoundsException()
+        else:
+            self._container[key] = data
+
+    def __iter__(self):
+        self._iter_index = 0
+        return self
+
+    def next(self): # __next__ if python3
+        """Defines behaviour for iterating over objects of this class."""
+        if self._iter_index == self.length():
+            raise StopIteration
+
+        if self._container[self._iter_index] is None:
+            raise self.NonePointerException()
+        else:
+            self._iter_index += 1
+            return self._container[self._iter_index - 1]
+
+    def length(self):
+        """Returns maximum capacity of the array."""
+        return len(self._container)
+
+    class ArrayOutOfBoundsException(Exception):
+        pass
+
+
+    class NonePointerException(Exception):
+        pass
+
+
+    class NegativeArraySizeException(Exception):
+        pass
+
+
+    class DimensionsOutOfBoundsException(Exception):
+        pass
+
+    class ArraySizeMissingException(Exception):
+        pass
